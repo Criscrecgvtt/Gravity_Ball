48b6dab44d3eb060a2670e3d757533e8ea23295d
Criscrecgvtt <crismendez.mir@gmail.com>
2025-07-07 19:18:53 +0200

    cambios bouncing_ball_accl

--- Code/bouncing_ball_accl.cpp
+++ Code/bouncing_ball_accl.cpp
@@ -6,11 +6,11 @@ int main()
 {
     //Definicion variables ventana
     unsigned int windowsize = 1000;
-    const int max_x = windowsize, max_y = windowsize, min_x = 0, min_y = 0;
+    const int max_x = windowsize, max_y = windowsize/2, min_x = 0, min_y = 0;
     float radius = 10;
     float x = 500.f-radius;
-    float y =500.f-radius;
-    int mov_x = 5, mov_y = 5;
+    float y =900;
+    int mov_x = 0, mov_y = -3;
 
    //Creo ventana y circulo
     sf::Vector2f position(x, y);
@@ -31,12 +31,13 @@ int main()
         x = position.x; 
         y = position.y;
        
-          if (x == max_x - radius * 2 || x < min_x){//hay que tener en cuenta el diametro del circulo apra calular que no se salga
-            mov_x *= -1;
-          }
-          if (y == max_y - radius * 2 || y < min_y){
+          if (y == min_y){//parte superior que es 0
             mov_y *= -1;
           }
+           if (y == windowsize){//inf
+            mov_y *= -1;
+          }
+          
 
         //pasamos las coordenadas actuales (x,y) al objeto circulo1
         sf::Vector2f movePos(mov_x, mov_y);
